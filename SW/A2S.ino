2884ab09c30ae8e26a5a4564e36308dca2b36325
DStage <97108789+DStageGarage@users.noreply.github.com>
2024-10-12 19:32:19 +0200

    corrections for glitches and staging
    Some small changes that seem to help to put speedo to 0 in abnormal situations like disconnection of the speed sensor, perhaps also smoother for normal low/0 speed.
    Also small change for staging, I had a report that it didn't work before but I haven't checked. For sure it should work now.

--- SW/A2S.ino
+++ SW/A2S.ino
@@ -1,7 +1,7 @@
 // =========================================================================================
 // === DStage A2S - ABS to Speed adapter ===================================================
 // =========================================================================================
-// Version 0.3 2024.09.04
+// Version 0.5 2024.10.10
 // Designed for Arduino Nano V3 with ATMega328PB and DStage A2S v3.0 hardware
 // IMPORTANT: this program will NOT WORK with ATMega328P!!!
 // 
@@ -15,9 +15,9 @@
 // Set to desired frequency recalculation factor if dipswitch is not used
 #define HARDCODED_FACTOR 5.67
 
-// Set to 1 if speed limit is to be used (requires setting the speedometr limit!)
+// Set to 1 if speed limit is to be used (requires setting the speedometr limit MAX_SPEEDO_FREQ!)
 #define USE_SPEEDO_LIMIT 0
-// Set to 1 if staging is to TURNED ON (requires setting the speedometr limit!)
+// Set to 1 if staging is to be TURNED ON (requires setting the speedometr limit MAX_SPEEDO_FREQ!)
 #define USE_STAGING 0
 // Set maximum output frequency in Hz at which speedometer reaches its limit if limit and/or staging is to be used
 #define MAX_SPEEDO_FREQ 205
@@ -53,33 +53,33 @@ float factor;
 
 void setup() 
 {
-  cli();                                              // globaly turn off the interrupts
+  cli();                                                // globaly turn off the interrupts
 
   #if USE_DIPSWITCH == 0
-    factor = HARDCODED_FACTOR/2;                      // hardcoded factor
+    factor = HARDCODED_FACTOR/2;                        // hardcoded factor
   #else
-    factor = dipswitch_setup_and_read();              // dipswitch defined factor - read at startup
+    factor = dipswitch_setup_and_read();                // dipswitch defined factor - read at startup
   #endif
 
-  period = 0;                                         // set initial frequency to 0
-  speed_out_setup();                                  // and init the output
+  period = 0;                                           // set initial frequency to 0
+  speed_out_setup();                                    // and init the output
 
   // if staging is to be used
   #if USE_STAGING == 1
-    delay(PRE_STAGING_DELAY);                         // wait before moving the needle
+    period = (float)MIN_HALF_PERIOD / factor;           // set max speed frequency
+    delay(PRE_STAGING_DELAY);                           // wait before moving the needle
     
-    sei();                                            // globaly turn on the interrupts to allow output signal to work
-    period = (float)MIN_HALF_PERIOD / factor;         // set max speed frequency
-    delay(MID_STAGING_DELAY);                         // and wait for a while
+    sei();                                              // globaly turn on the interrupts to allow output signal to work
+    delay(MID_STAGING_DELAY);                           // and wait for a while
 
-    period = 0;                                       // set speed to 0
-    delay(POST_STAGING_DELAY);                        // and wait for a while
-    cli();                                            // globaly turn off the interrupts to setup input in peace
+    period = 0;                                         // set speed to 0
+    delay(POST_STAGING_DELAY);                          // and wait for a while
+    cli();                                              // globaly turn off the interrupts to setup input in peace
   #endif
 
-  speed_in_setup();                                   // setup the input
+  speed_in_setup();                                     // setup the input
   
-  sei();                                              // globaly turn on the interrupts
+  sei();                                                // globaly turn on the interrupts
 }
 
 
@@ -96,15 +96,15 @@ void loop()
 // =========================================================================================
 void speed_in_setup()
 {
-  TCCR3A = (0 << 6) | (0 << 4) | (0 << 0);            // Normal mode, top 0xFFFF, no output
-  TCCR3B = (1 << 7) | (1 << 6) | (0 << 3) | (4 << 0); // Normal mode, top 0xFFFF, noice canceler, rising edge, prescaler 256 
+  TCCR3A = (0 << 6) | (0 << 4) | (0 << 0);              // Normal mode, top 0xFFFF, no output
+  TCCR3B = (1 << 7) | (1 << 6) | (0 << 3) | (4 << 0);   // Normal mode, top 0xFFFF, noice canceler, rising edge, prescaler 256 
   TCCR3C = 0;
-  TIMSK3 = (1 << 5) | (1 << 0);                       // input capture interrupt, overflow interrupt
+  TIMSK3 = (1 << 5) | (1 << 0);                         // input capture interrupt, overflow interrupt
 
-  PORTE &= ~(1 << 2);                                 // no pull-up on PE2
-  DDRE &= ~(1 << 2);                                  // PE2 as input ICP3
+  PORTE &= ~(1 << 2);                                   // no pull-up on PE2
+  DDRE &= ~(1 << 2);                                    // PE2 as input ICP3
 
-  t3_ovf = 0;                                         // clear overflow count
+  t3_ovf = 0;                                           // clear overflow count
   period = 0;
 }
 
@@ -113,23 +113,26 @@ ISR(TIMER3_CAPT_vect)
 {
   if(t3_ovf > 1)
   {
-    timestamp = ICR3;                                 // save new timestamp
+    //timestamp = ICR3;                                 // save new timestamp - moved to the end
     // clear overflow and wait till next event
     t3_ovf = 0;
-    return;
-  }
-
-  if(t3_ovf == 1)
-  {
-    // if there was one overflow calculate time from last capture to verflow and add current capture
-    period = (0xFFFF - timestamp) + ICR3;
-    t3_ovf = 0;
+    //return;                                           // exit interrupt - swapped to else statement
   }
   else
   {
-    period = ICR3 - timestamp;  
+    if(t3_ovf == 1)
+    {
+      // if there was one overflow calculate time from last capture to overflow and add current capture
+      period = (0xFFFF - timestamp) + ICR3;
+      t3_ovf = 0;
+    }
+    else
+    {
+      period = ICR3 - timestamp;  
+    }
+    //timestamp = ICR3;                                   // save new timestamp - moved to the end
   }
-  timestamp = ICR3;                                   // save new timestamp
+  timestamp = ICR3;                                     // save new timestamp
 }
 
 // timer overflow is normal in this mode but happening twice means no movement
@@ -137,8 +140,11 @@ ISR(TIMER3_OVF_vect)
 {
   // increment overflow count
   t3_ovf++;
-  if(t3_ovf > 100)
-    t3_ovf = 100;
+  if(t3_ovf > 1)
+  {
+    t3_ovf = 2;
+    period = 0;                                         // redundand to main loop, just in case, unwanted behaviour possible without it with glithces at the input
+  }
 }
 // =========================================================================================
 
@@ -149,16 +155,16 @@ ISR(TIMER3_OVF_vect)
 void speed_out_setup()
 {
   OCR4A = 0x0000;
-  TCCR4A = (0 << 6) | (0 << 4) | (0 << 0);            // CTC mode, top OCR4A, no output
-  TCCR4B = (0 << 7) | (0 << 6) | (1 << 3) | (4 << 0); // CTC mode, top OCR4A, prescaler 256 
+  TCCR4A = (0 << 6) | (0 << 4) | (0 << 0);              // CTC mode, top OCR4A, no output
+  TCCR4B = (0 << 7) | (0 << 6) | (1 << 3) | (4 << 0);   // CTC mode, top OCR4A, prescaler 256 
   TCCR4C = 0;
-  TIMSK4 = (0 << 5) | (1 << 1);                       // output compare A interrupt
+  TIMSK4 = (0 << 5) | (1 << 1);                         // output compare A interrupt
 
-  PORTD &= ~(1 << 2);                                 // L state on PD2
-  DDRD |= 1 << 2;                                     // PD2 as outout for speed
+  PORTD &= ~(1 << 2);                                   // L state on PD2
+  DDRD |= 1 << 2;                                       // PD2 as outout for speed
 
-  PORTB &= ~(1 << 5);                                 // L state on PB5
-  DDRB |= 1 << 5;                                     // PB5 as Arduino Nano build in LED outout
+  PORTB &= ~(1 << 5);                                   // L state on PB5
+  DDRB |= 1 << 5;                                       // PB5 as Arduino Nano build in LED outout
 }
 
 
@@ -169,24 +175,27 @@ ISR(TIMER4_COMPA_vect)
 
   if(period == 0)
   {
-    PORTD &= ~(1 << 2);                               // PD2 state L, speed 0km/h
-    OCR4A = 0x8FFF;                                   // next check in half of maximum period
-    return;                                           // exit interrupt
+    //PORTD &= ~(1 << 2);                               // PD2 state L, speed 0km/h
+    // leave output state as it is without transition
+    OCR4A = 0x8FFF;                                     // next check in half of maximum period
+    //return;                                           // exit interrupt - swapped to else statement
   }
+  else 
+  {
+    PORTD ^= (1 << 2);                                  // toggle PD2 output to speedometer
+    PORTB ^= (1 << 5);                                  // toggle PB5 - LED
 
-  PORTD ^= (1 << 2);                                  // toggle PD2 output to speedometer
-  PORTB ^= (1 << 5);                                  // toggle PB5 - LED
+    // calculate next half period
+    temp_half_period = (float)period * factor;
+    
+    // if speedo limit is to be used
+    #if USE_SPEEDO_LIMIT == 1
+      if(temp_half_period < MIN_HALF_PERIOD)            // if speed higher than speedo limit
+        temp_half_period = MIN_HALF_PERIOD;             // limit the output frequency
+    #endif
 
-  // calculate next half period
-  temp_half_period = (float)period * factor;
-  
-  // if speedo limit is to be used
-  #if USE_SPEEDO_LIMIT == 1
-    if(temp_half_period < MIN_HALF_PERIOD)            // if speed higher than speedo limit
-      temp_half_period = MIN_HALF_PERIOD;             // limit the output frequency
-  #endif
-
-  OCR4A = temp_half_period;
+    OCR4A = temp_half_period;
+  }
 }
 // =========================================================================================
 
@@ -220,9 +229,9 @@ ISR(TIMER4_COMPA_vect)
     temp |= digitalRead(SET7) << 7;
     temp |= digitalRead(SET8) << 8;
     temp |= digitalRead(SET9) << 9;
-    temp ^= 0x3FF;                                    // negate the 10bit value as dipswitch is active low
+    temp ^= 0x3FF;                                      // negate the 10bit value as dipswitch is active low
 
-    return (float)temp * 0.005;                       // factor equals dipswitch decimal value * 0.01 but internaly factor has to be divide by 2 thus 0.005
+    return (float)temp * 0.005;                         // factor equals dipswitch decimal value * 0.01 but internaly factor has to be divided by 2 thus 0.005
   }
 #endif
 // =========================================================================================
